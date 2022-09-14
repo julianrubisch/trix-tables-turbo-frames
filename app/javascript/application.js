61ba6ff948f9418385b0e3c992e4527462416c14
Julian Rubisch <julian@julianrubisch.at>
2022-09-14 17:05:23 +0200

    chore: Customize Trix toolbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-import "@rails/actiontext"
+import "@hotwired/turbo-rails";
+import "controllers";
+import Trix from "trix";
+import "@rails/actiontext";
+
+const buttonHTML =
+  '<button type="button" class="trix-button trix-button--icon trix-button--icon-table" title="table" tabindex="-1"  data-action="trix-table#attachTable">table</button>';
+
+const buttonGroupElement = document
+  .querySelector("trix-editor")
+  .toolbarElement.querySelector("[data-trix-button-group=file-tools]");
+
+buttonGroupElement.insertAdjacentHTML("beforeend", buttonHTML);
